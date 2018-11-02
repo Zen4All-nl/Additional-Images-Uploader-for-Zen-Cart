8a9d32a1f684e751a533608344205945f997ea4a
Zen4All <info@zen4all.nl>
2018-11-02 09:55:15 +0100

    bump version

---
+++ YOUR_ADMIN/includes/installers/additional_images_uploader/1_2_0.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  1_2_0.php
+ *
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version Author: Zen4All  2/11/2018
+ */
+
+// Major overhaul
+
